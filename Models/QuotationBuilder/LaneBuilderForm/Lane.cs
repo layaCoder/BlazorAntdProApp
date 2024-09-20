f69dc935c4243d755b769a5a996c65291fd043d3
Jason <412046213@qq.com>
2024-09-20 10:31:59 +0800

    feat: add inline form in LaneBuilderForm component

---
+++ Models/QuotationBuilder/LaneBuilderForm/Lane.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAntdProApp.Models.QuotationBuilder.LaneBuilderForm
+{
+    public class Lane
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        public string Carrier { get; set; }
+        public string OriginPort { get; set; }
+
+        public string DestinationPort { get; set; }
+
+        public string Routing { get; set; }
+
+        public string DestinationDoor { get; set; }
+
+        public int DoorRate { get; set; }
+
+    }
+}
