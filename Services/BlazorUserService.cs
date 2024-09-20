5378717a5b57082c362fe1917b2eb0687372ec0d
Jason <412046213@qq.com>
2024-09-20 17:19:57 +0800

    feat: remove useless UI components

---
+++ Services/BlazorUserService.cs
@@ -0,0 +1,23 @@
+﻿using BlazorAntdProApp.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAntdProApp.Services
+{
+    public interface IBlazorUserService
+    {
+        //Task<HttpResponseMessage> getAllBlazorUser();
+    }
+
+    public class BlazorUserService:IBlazorUserService
+    {
+        //private readonly HttpClient _httpClient;
+
+  
+
+
+        //public async Task<HttpResponseMessage> getAllBlazorUser()
+        //{
+        //    return await _httpClient.GetAsync("https://www.layablog.top/api/get/whatsHot");
+        //}
+    }
+}
