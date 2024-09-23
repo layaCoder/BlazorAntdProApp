8bffc0409f3b2237ab4f4612ea6b0be2bfc95ff9
Jason <412046213@qq.com>
2024-09-23 17:11:08 +0800

    feat: add customize layout

---
+++ Layouts/CustomizeLayout.razor.cs
@@ -0,0 +1,22 @@
+﻿using AntDesign.Extensions.Localization;
+using AntDesign.ProLayout;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorAntdProApp.Layouts
+{
+   
+
+    public partial class CustomizeLayout:LayoutComponentBase
+    {
+        private MenuDataItem[] _menuData =[];
+        [Inject] private HttpClient HttpClient { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            
+            _menuData = await HttpClient.GetFromJsonAsync<MenuDataItem[]>("data/menu.json");
+
+        }
+    }
+}
