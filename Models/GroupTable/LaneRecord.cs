5fc9c9cfcfaf4f0f112196a32515fbb9a8838020
Jason <412046213@qq.com>
2024-09-25 09:31:56 +0800

    feat: add dataTransform function in GroupTable component

---
+++ Models/GroupTable/LaneRecord.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAntdProApp.Models.GroupTable
+{
+    public class LaneRecord
+    {
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Carrier { get; set; }
+        public string OriginPort { get; set; }
+
+        public string DestinationPort { get; set; }
+
+        public string Routing { get; set; }
+
+        public string DestinationDoor { get; set; }
+
+        public int DoorRate { get; set; }
+
+        public string Door { get; set; }
+
+        public int Transit { get; set; }
+        public string Commodity { get; set; }
+        public string ContainerSize { get; set; }
+        public int NewOceanFreight { get; set; }
+        public int CurrentOceanFreight {  set; get; }
+        public int NewBafLss { get; set; }
+        public int CurrentBafLss { set; get; }
+        public int NewDps { get; set; }
+        public int CurrentDps { set; get;}
+        public int TMF { get; set; }
+        public int ISP { get; set; }
+        public int AMS { get; set; }
+        public int ADD { get; set; }
+        public int ARR { get; set; }
+        public int PSS { get; set; }
+        public int CRF { get; set; }
+        public int NewTotal { get; set; }
+        public int CurrentTotal { get; set; }
+
+    }
+}
