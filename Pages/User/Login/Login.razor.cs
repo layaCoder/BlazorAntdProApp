bc3460ccfe78db59d9f7ed76ba1d3c384124fa67
Jason <412046213@qq.com>
2024-09-29 10:10:57 +0800

    feat: set opacity in login image

--- Pages/User/Login/Login.razor.cs
+++ Pages/User/Login/Login.razor.cs
@@ -12,26 +12,15 @@ namespace BlazorAntdProApp.Pages.User
 
         [Inject] public NavigationManager NavigationManager { get; set; }
 
-        [Inject] public IAccountService AccountService { get; set; }
-
-        [Inject] public MessageService Message { get; set; }
 
         public void HandleSubmit()
         {
             if (_model.UserName == "admin" && _model.Password == "admin")
             {
                 NavigationManager.NavigateTo("/");
-                //NavigationManager.NavigateTo("/list/search/articles");
                 return;
             }
-
-            if (_model.UserName == "user" && _model.Password == "ant.design") NavigationManager.NavigateTo("/");
         }
 
-        public async Task GetCaptcha()
-        {
-            var captcha = await AccountService.GetCaptchaAsync(_model.Mobile);
-            await Message.Success($"Verification code validated successfully! The verification code is: {captcha}");
-        }
     }
 }
