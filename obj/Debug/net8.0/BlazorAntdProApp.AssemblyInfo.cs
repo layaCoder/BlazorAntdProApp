4bc24af1044e89e9224c937fc7091d544fa8ad05
Jason <412046213@qq.com>
2024-09-29 17:27:44 +0800

    style: update theme color

--- obj/Debug/net8.0/BlazorAntdProApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorAntdProApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAntdProApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099edcfc5166ea16c70b4aefb0999cf0067770ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAntdProApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAntdProApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
